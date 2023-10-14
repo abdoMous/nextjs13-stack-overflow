c9b3ede341000cd05b88c78d48a87587cc03e5d3
Abderrahmane Moussaoui <msiabdrahmn@gmail.com>
2023-10-14 16:25:41 +0100

    randar tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
 
 const hotQuestions = [
   {
@@ -24,6 +26,14 @@ const hotQuestions = [
   },
 ];
 
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 2 },
+  { _id: 3, name: "next", totalQuestions: 4 },
+  { _id: 4, name: "mongo", totalQuestions: 6 },
+  { _id: 5, name: "android", totalQuestions: 1 },
+];
+
 const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
@@ -39,12 +49,32 @@ const RightSidebar = () => {
                 <p className="body-medium text-dark500_light700 ">
                   {question.title}
                 </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="chevron right"
+                  width={20}
+                  height={20}
+                  className="invert-colors"
+                />
               </Link>
             );
           })}
         </div>
       </div>
-      <div className="mt-16">test2</div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
