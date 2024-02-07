818614738fbd05045ed08edf9b56a0d7e6c00463
Abderrahmane Moussaoui <msiabdrahmn@gmail.com>
2024-02-07 16:53:23 +0100

    Refactor RightSidebar component and add  fetch hot questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,82 +1,63 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import RenderTag from "./RenderTag";
-
-const hotQuestions = [
-  {
-    _id: "1",
-    title: "how do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: "2",
-    title: "how do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: "3",
-    title: "how do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: "4",
-    title: "how do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: "5",
-    title: "how do I use express as a custom server in NextJS?",
-  },
-];
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import RenderTag from './RenderTag';
+import { getHotQuestions } from '@/lib/actions/question.action';
 
 const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "react", totalQuestions: 2 },
-  { _id: "3", name: "next", totalQuestions: 4 },
-  { _id: "4", name: "mongo", totalQuestions: 6 },
-  { _id: "5", name: "android", totalQuestions: 1 },
+    { _id: '1', name: 'javascript', totalQuestions: 5 },
+    { _id: '2', name: 'react', totalQuestions: 2 },
+    { _id: '3', name: 'next', totalQuestions: 4 },
+    { _id: '4', name: 'mongo', totalQuestions: 6 },
+    { _id: '5', name: 'android', totalQuestions: 1 },
 ];
 
-const RightSidebar = () => {
-  return (
-    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="">
-        <h3 className="h3-bold text-dark200_light900">Top Question</h3>
-        <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestions.map((question) => {
-            return (
-              <Link
-                href="/"
-                key={question._id}
-                className="flex cursor-pointer items-center justify-between gap-7">
-                <p className="body-medium text-dark500_light700 ">
-                  {question.title}
-                </p>
-                <Image
-                  src="/assets/icons/chevron-right.svg"
-                  alt="chevron right"
-                  width={20}
-                  height={20}
-                  className="invert-colors"
-                />
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-      <div className="mt-16">
-        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
-        <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map((tag) => (
-            <RenderTag
-              key={tag._id}
-              _id={tag._id}
-              name={tag.name}
-              totalQuestions={tag.totalQuestions}
-              showCount
-            />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+const RightSidebar = async () => {
+    const hotQuestions = await getHotQuestions();
+    // const popularTags = await getPopularTags();
+
+    return (
+        <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+            <div className="">
+                <h3 className="h3-bold text-dark200_light900">Top Question</h3>
+                <div className="mt-7 flex w-full flex-col gap-[30px]">
+                    {hotQuestions.map((question) => {
+                        return (
+                            <Link
+                                href={`/question/${question._id}`}
+                                key={question._id}
+                                className="flex cursor-pointer items-center justify-between gap-7">
+                                <p className="body-medium text-dark500_light700 ">
+                                    {question.title}
+                                </p>
+                                <Image
+                                    src="/assets/icons/chevron-right.svg"
+                                    alt="chevron right"
+                                    width={20}
+                                    height={20}
+                                    className="invert-colors"
+                                />
+                            </Link>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="mt-16">
+                <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+                <div className="mt-7 flex flex-col gap-4">
+                    {popularTags.map((tag) => (
+                        <RenderTag
+                            key={tag._id}
+                            _id={tag._id}
+                            name={tag.name}
+                            totalQuestions={tag.totalQuestions}
+                            showCount
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default RightSidebar;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,277 +1,315 @@
-"use server";
+'use server';
 
-import Question from "@/database/question.model";
-import { connectToDatabase } from "../mongoose";
-import Tag, { ITag } from "@/database/tag.model";
+import Question from '@/database/question.model';
+import { connectToDatabase } from '../mongoose';
+import Tag, { ITag } from '@/database/tag.model';
 import {
-  CreateQuestionParams,
-  DeleteQuestionParams,
-  EditQuestionParams,
-  GetQuestionByIdParams,
-  GetQuestionsByTagIdParams,
-  GetQuestionsParams,
-  GetSavedQuestionsParams,
-  GetUserStatsParams,
-  QuestionVoteParams,
-} from "./shared.types";
-import User from "@/database/user.model";
-import { revalidatePath } from "next/cache";
-import { FilterQuery } from "mongoose";
-import Answer from "@/database/answer.model";
-import Interaction from "@/database/interaction.model";
+    CreateQuestionParams,
+    DeleteQuestionParams,
+    EditQuestionParams,
+    GetQuestionByIdParams,
+    GetQuestionsByTagIdParams,
+    GetQuestionsParams,
+    GetSavedQuestionsParams,
+    GetUserStatsParams,
+    QuestionVoteParams,
+} from './shared.types';
+import User from '@/database/user.model';
+import { revalidatePath } from 'next/cache';
+import { FilterQuery } from 'mongoose';
+import Answer from '@/database/answer.model';
+import Interaction from '@/database/interaction.model';
 
 export async function getQuestions(params: GetQuestionsParams) {
-  // const { page = 1, pageSize = 10, searchQuery = "", filter = "" } = params;
-  try {
-    connectToDatabase();
-    const questions = await Question.find({})
-      .maxTimeMS(100000)
-      .populate({ path: "tags", model: Tag })
-      .populate({ path: "author", model: User })
-      .sort({ createdAt: -1 });
+    // const { page = 1, pageSize = 10, searchQuery = "", filter = "" } = params;
+    try {
+        connectToDatabase();
+        const questions = await Question.find({})
+            .maxTimeMS(100000)
+            .populate({ path: 'tags', model: Tag })
+            .populate({ path: 'author', model: User })
+            .sort({ createdAt: -1 });
 
-    return questions;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+        return questions;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
 
 export async function getSavedQuestions(params: GetSavedQuestionsParams) {
-  // const { page = 1, pageSize = 10, searchQuery = "", filter = "" } = params;
-  try {
-    connectToDatabase();
+    // const { page = 1, pageSize = 10, searchQuery = "", filter = "" } = params;
+    try {
+        connectToDatabase();
 
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+        const {
+            clerkId,
+            page = 1,
+            pageSize = 10,
+            filter,
+            searchQuery,
+        } = params;
 
-    const query: FilterQuery<typeof Question> = searchQuery
-      ? { title: { $regex: new RegExp(searchQuery, "i") } }
-      : {};
+        const query: FilterQuery<typeof Question> = searchQuery
+            ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+            : {};
 
-    const user = await User.findOne({ clerkId }).populate({
-      path: "saved",
-      match: query,
-      options: {
-        sort: { createdAt: -1 },
-      },
-      populate: [
-        { path: "tags", model: Tag, select: "_id name" },
-        { path: "author", model: User, select: "_id clerkId name picture" },
-      ],
-    });
+        const user = await User.findOne({ clerkId }).populate({
+            path: 'saved',
+            match: query,
+            options: {
+                sort: { createdAt: -1 },
+            },
+            populate: [
+                { path: 'tags', model: Tag, select: '_id name' },
+                {
+                    path: 'author',
+                    model: User,
+                    select: '_id clerkId name picture',
+                },
+            ],
+        });
 
-    if (!user) {
-      throw new Error("User not found");
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return user.saved;
+    } catch (error) {
+        console.error(error);
+        return [];
     }
-
-    return user.saved;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
 }
 
 export async function createQuestion(params: CreateQuestionParams) {
-  try {
-    connectToDatabase();
-    const { title, content, tags, author, path } = params;
+    try {
+        connectToDatabase();
+        const { title, content, tags, author, path } = params;
 
-    // Create the question
-    const question = await Question.create({
-      title,
-      content,
-      author,
-    });
+        // Create the question
+        const question = await Question.create({
+            title,
+            content,
+            author,
+        });
 
-    const tagDocuments = [];
+        const tagDocuments = [];
 
-    for (const tag of tags) {
-      const existingTag = await Tag.findOneAndUpdate(
-        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
-        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
-        { upsert: true, new: true }
-      );
+        for (const tag of tags) {
+            const existingTag = await Tag.findOneAndUpdate(
+                { name: { $regex: new RegExp(`^${tag}$`, 'i') } },
+                {
+                    $setOnInsert: { name: tag },
+                    $push: { questions: question._id },
+                },
+                { upsert: true, new: true }
+            );
 
-      tagDocuments.push(existingTag._id);
+            tagDocuments.push(existingTag._id);
+        }
+
+        await Question.findByIdAndUpdate(question._id, {
+            $push: { tags: { $each: tagDocuments } },
+        });
+
+        // Create an interaction record for the users's ask_question action
+        // Increment author's  reputation by +5 for creating a question
+
+        revalidatePath(path);
+    } catch (error) {
+        console.log(error);
     }
-
-    await Question.findByIdAndUpdate(question._id, {
-      $push: { tags: { $each: tagDocuments } },
-    });
-
-    // Create an interaction record for the users's ask_question action
-    // Increment author's  reputation by +5 for creating a question
-
-    revalidatePath(path);
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 export async function getQuestionById(params: GetQuestionByIdParams) {
-  try {
-    connectToDatabase();
-    const question = await Question.findById(params.questionId)
-      .maxTimeMS(100000)
-      .populate({ path: "tags", model: Tag, select: "_id name" })
-      .populate({
-        path: "author",
-        model: User,
-        select: "_id clerkId name picture",
-      });
+    try {
+        connectToDatabase();
+        const question = await Question.findById(params.questionId)
+            .maxTimeMS(100000)
+            .populate({ path: 'tags', model: Tag, select: '_id name' })
+            .populate({
+                path: 'author',
+                model: User,
+                select: '_id clerkId name picture',
+            });
 
-    return question;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+        return question;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 export async function voteQuestion(params: QuestionVoteParams) {
-  try {
-    connectToDatabase();
+    try {
+        connectToDatabase();
 
-    const { questionId, userId, voteType, hasupVoted, hasdownVoted, path } =
-      params;
-    let updateQuery = {};
+        const { questionId, userId, voteType, hasupVoted, hasdownVoted, path } =
+            params;
+        let updateQuery = {};
 
-    if (voteType === "upvote") {
-      if (hasupVoted) {
-        updateQuery = { $pull: { upvotes: userId } };
-      } else if (hasdownVoted) {
-        updateQuery = {
-          $pull: { downvotes: userId },
-          $push: { upvotes: userId },
-        };
-      } else {
-        updateQuery = { $addToSet: { upvotes: userId } };
-      }
-    } else if (voteType === "downvote") {
-      if (hasdownVoted) {
-        updateQuery = { $pull: { downvotes: userId } };
-      } else if (hasupVoted) {
-        updateQuery = {
-          $pull: { upvotes: userId },
-          $push: { downvotes: userId },
-        };
-      } else {
-        updateQuery = { $addToSet: { downvotes: userId } };
-      }
+        if (voteType === 'upvote') {
+            if (hasupVoted) {
+                updateQuery = { $pull: { upvotes: userId } };
+            } else if (hasdownVoted) {
+                updateQuery = {
+                    $pull: { downvotes: userId },
+                    $push: { upvotes: userId },
+                };
+            } else {
+                updateQuery = { $addToSet: { upvotes: userId } };
+            }
+        } else if (voteType === 'downvote') {
+            if (hasdownVoted) {
+                updateQuery = { $pull: { downvotes: userId } };
+            } else if (hasupVoted) {
+                updateQuery = {
+                    $pull: { upvotes: userId },
+                    $push: { downvotes: userId },
+                };
+            } else {
+                updateQuery = { $addToSet: { downvotes: userId } };
+            }
+        }
+
+        const question = await Question.findByIdAndUpdate(
+            questionId,
+            updateQuery,
+            {
+                new: true,
+            }
+        );
+
+        if (!question) {
+            throw new Error('Question not found');
+        }
+        // todo increment author's reputation
+        revalidatePath(path);
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
-      new: true,
-    });
-
-    if (!question) {
-      throw new Error("Question not found");
-    }
-    // todo increment author's reputation
-    revalidatePath(path);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 }
 
 export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
-  try {
-    connectToDatabase();
-    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
-    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+    try {
+        connectToDatabase();
+        const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+        const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
-    const tag = await Tag.findOne(tagFilter).populate({
-      path: "questions",
-      model: Question,
-      match: searchQuery
-        ? { title: { $regex: new RegExp(searchQuery, "i") } }
-        : {},
-      options: {
-        sort: { createdAt: -1 },
-      },
-      populate: [
-        { path: "tags", model: Tag, select: "_id name" },
-        { path: "author", model: User, select: "_id clerkId name picture" },
-      ],
-    });
+        const tag = await Tag.findOne(tagFilter).populate({
+            path: 'questions',
+            model: Question,
+            match: searchQuery
+                ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+                : {},
+            options: {
+                sort: { createdAt: -1 },
+            },
+            populate: [
+                { path: 'tags', model: Tag, select: '_id name' },
+                {
+                    path: 'author',
+                    model: User,
+                    select: '_id clerkId name picture',
+                },
+            ],
+        });
 
-    if (!tag) {
-      throw new Error("Tag not found");
+        if (!tag) {
+            throw new Error('Tag not found');
+        }
+
+        return { tagTitle: tag.name, questions: tag.questions };
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    return { tagTitle: tag.name, questions: tag.questions };
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 }
 
 export async function getUserQuestions(params: GetUserStatsParams) {
-  try {
-    connectToDatabase();
+    try {
+        connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
-    const totalQuestions = await Question.countDocuments({ author: userId });
-    const userQuestions = await Question.find({ author: userId })
-      .sort({
-        views: -1,
-        upvotes: -1,
-      })
-      .populate("tags", "_id name")
-      .populate("author", "_id clerkId name picture");
+        const { userId, page = 1, pageSize = 10 } = params;
+        const totalQuestions = await Question.countDocuments({
+            author: userId,
+        });
+        const userQuestions = await Question.find({ author: userId })
+            .sort({
+                views: -1,
+                upvotes: -1,
+            })
+            .populate('tags', '_id name')
+            .populate('author', '_id clerkId name picture');
 
-    return { totalQuestions, questions: userQuestions };
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+        return { totalQuestions, questions: userQuestions };
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 export async function deleteQuestion(params: DeleteQuestionParams) {
-  try {
-    connectToDatabase();
-    const { questionId, path } = params;
+    try {
+        connectToDatabase();
+        const { questionId, path } = params;
 
-    await Question.findByIdAndDelete(questionId);
-    await Answer.deleteMany({ question: questionId });
-    await Interaction.deleteMany({ question: questionId });
-    await Tag.updateMany(
-      { questions: questionId },
-      { $pull: { questions: questionId } }
-    );
+        await Question.findByIdAndDelete(questionId);
+        await Answer.deleteMany({ question: questionId });
+        await Interaction.deleteMany({ question: questionId });
+        await Tag.updateMany(
+            { questions: questionId },
+            { $pull: { questions: questionId } }
+        );
 
-    revalidatePath(path);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+        revalidatePath(path);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 export async function editQuestion(params: EditQuestionParams) {
-  try {
-    connectToDatabase();
+    try {
+        connectToDatabase();
 
-    const { questionId, title, content, path } = params;
+        const { questionId, title, content, path } = params;
 
-    const question = await Question.findByIdAndUpdate(questionId).populate({
-      path: "tags",
-      model: Tag,
-    });
+        const question = await Question.findByIdAndUpdate(questionId).populate({
+            path: 'tags',
+            model: Tag,
+        });
 
-    if (!question) {
-      throw new Error("Question not found");
+        if (!question) {
+            throw new Error('Question not found');
+        }
+
+        question.title = title;
+        question.content = content;
+
+        await question.save();
+
+        revalidatePath(path);
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
+}
 
-    question.title = title;
-    question.content = content;
+export async function getHotQuestions() {
+    try {
+        connectToDatabase();
 
-    await question.save();
+        const hotQuestions = await Question.find({})
+            .sort({ views: -1, upvotes: -1 }) // decending order
+            .limit(5);
 
-    revalidatePath(path);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+        return hotQuestions;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 // export async function getAllUsers(params: GetAllUsersParams) {
